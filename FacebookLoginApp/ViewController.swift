4daa50c2d2c2f592b686e7775c98622f31f30a8e
Chhaya Tiwari <apple@Chhaya-Tiwari.local>
2018-10-09 00:54:50 +0530

    Presented Login Screen and graph Api request

--- FacebookLoginApp/ViewController.swift
+++ FacebookLoginApp/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
@@ -19,8 +21,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loginWithFacebook(_ sender: Any) {
-        
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [], viewController: self) { (loginResult: LoginResult) in
+            switch loginResult {
+            case .failed(let error):
+                print(error.localizedDescription)
+            case .cancelled :
+                print("Cancelled")
+            case .success(grantedPermissions: _, declinedPermissions: _, token: _):
+                print("login succes")
+                self.getDetail()
+            }
+        }
     }
     
+    func getDetail() {
+        
+        guard let accessToken = AccessToken.current else {
+            return
+        }
+        let parameter = ["fields" : "name, picture"]
+        let graphRequest = GraphRequest(graphPath: "me", parameters: parameter, accessToken: accessToken)
+        graphRequest.start { (urlResponse, requestResult) in
+            switch requestResult {
+            case .failed(let error):
+                print(error.localizedDescription)
+            case .success(response: let graphResponse):
+                print(graphResponse.dictionaryValue)
+            }
+        }
+    }
 }
 
